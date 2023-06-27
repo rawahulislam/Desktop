02747bdeab47c806cd7963777035c65f7c161ca3
rawahulislam <8715096926.abd@gmail.com>
2023-06-27 16:13:21 +0530

    ffe

--- lawfirm/lawfirm/src/App.js
+++ lawfirm/lawfirm/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
+import Navbar from './component/LandingPage/landingPage';
 import './App.css';
 
 function App() {
   return (
     <div>
+   <Navbar/>
     </div>
   );
 }
