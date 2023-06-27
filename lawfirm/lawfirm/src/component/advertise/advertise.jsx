fee2abb2babd271bf27530b52cfe9c00fca1b3d9
rawahulislam <8715096926.abd@gmail.com>
2023-06-27 23:13:22 +0530

    advertise

---
+++ lawfirm/lawfirm/src/component/advertise/advertise.jsx
@@ -0,0 +1,17 @@
+import "./advertise.css";
+
+export default function Advertise() {
+  return (
+    <div className="advertise">
+      <div className="text1">Let's introduce ourself</div>
+      <hr/>
+      <div className="typo">
+        <span className="text2">Criminal lawyer</span>
+        <span className="para">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequatduis enim velit mollit Exercitation.
+        </span>
+      </div>
+    </div>
+  );
+}
