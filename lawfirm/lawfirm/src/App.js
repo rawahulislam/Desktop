4c086da123da0123ffea840dc561db404190c9ef
rawahulislam <8715096926.abd@gmail.com>
2023-06-27 22:19:43 +0530

    updated_hero_section

--- lawfirm/lawfirm/src/App.js
+++ lawfirm/lawfirm/src/App.js
@@ -1,11 +1,13 @@
 
 import Navbar from './component/Navbar/navbar';
 import './App.css';
+import Hero from './component/heroSection/herojsx';
 
 function App() {
   return (
     <div>
    <Navbar/>
+   <Hero/>
     </div>
   );
 }
