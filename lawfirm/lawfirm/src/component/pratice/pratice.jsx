4b0a06f8e35b5f7113281179c01239f06b1aa002
rawahulislam <8715096926.abd@gmail.com>
2023-06-28 15:11:26 +0530

    update3

--- lawfirm/lawfirm/src/component/pratice/pratice.jsx
+++ lawfirm/lawfirm/src/component/pratice/pratice.jsx
@@ -1,39 +1,68 @@
-import "./pratice.css"
-import Image1 from "../../assets/img1.png"
-import Image2 from "../../assets/img2.png"
-import Image3 from "../../assets/img3.png"
-import Image4 from "../../assets/img4.png"
-import Image5 from "../../assets/img5.png"
-import Image6 from "../../assets/img6.png"
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import "./pratice.css";
+import Image1 from "../../assets/img1.png";
+import Image2 from "../../assets/img2.png";
+import Image3 from "../../assets/img3.png";
+import Image4 from "../../assets/img4.png";
+import Image5 from "../../assets/img5.png";
+import Image6 from "../../assets/img6.png";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
-
-export default function Pratice(){
-    return(<div className="containerPratice">
-      <div className="text1">Area of Pratice</div>
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={3}>
-        <Grid item xs={8}>
-          <img src={Image6} alt="img1"/>
+export default function Pratice() {
+  return (
+    <div className="containerPratice">
+      <div
+        className="text1"
+        style={{ display: "flex", justifyContent: "center", width: "auto" }}
+      >
+        Area of Pratices
+      </div>
+      <Box sx={{ flexGrow: 1, marginBottom:"6rem"}}>
+        <Grid container spacing={3}>
+          <Grid item xs={8}>
+            <img src={Image6} alt="img1" />
+            <span style={{ position: "absolute", top: "31rem", left: "1rem" }}>
+              Business Law
+            </span>
+          </Grid>
+          <Grid item xs={4}>
+            <img src={Image5} alt="img2" />
+            <span
+              style={{ position: "absolute", top: "31rem", left: "49.5rem" }}
+            >
+              Partnersip Law
+            </span>
+          </Grid>
+          <Grid item xs={4}>
+            <img src={Image4} alt="img3" />
+            <span style={{ position: "absolute", top: "54rem", left: "1rem" }}>
+              Real Estate Law
+            </span>
+          </Grid>
+          <Grid item xs={8}>
+            <img src={Image3} alt="img4" />
+            <span
+              style={{ position: "absolute", top: "54rem", left: "25.5rem" }}
+            >
+              Business Law
+            </span>
+          </Grid>
+          <Grid item xs={8}>
+            <img src={Image2} alt="img5" />
+            <span style={{ position: "absolute", top: "77rem", left: "1rem" }}>
+              Landlord Disputes
+            </span>
+          </Grid>
+          <Grid item xs={4}>
+            <img src={Image1} alt="img6" />
+            <span
+              style={{ position: "absolute", top: "77rem", left: "49.5rem" }}
+            >
+              Elder Abuse
+            </span>
+          </Grid>
         </Grid>
-        <Grid item xs={4}>
-        <img src={Image5} alt="img2"/>
-        </Grid>
-        <Grid item xs={4}>
-        <img src={Image4} alt="img3"/>
-        </Grid>
-        <Grid item xs={8}>
-        <img src={Image3} alt="img4"/>
-        </Grid>
-        <Grid item xs={8}>
-        <img src={Image2} alt="img5"/>
-        </Grid>
-        <Grid item xs={4}>
-        <img src={Image1} alt="img6"/>
-        </Grid>
-      </Grid>
-    </Box>
-    
-    </div>)
+      </Box>
+    </div>
+  );
 }
