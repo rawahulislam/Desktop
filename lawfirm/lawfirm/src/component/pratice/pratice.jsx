6a3cc9543eb848f69ffbd3f5acf4faef7c71336e
rawahulislam <8715096926.abd@gmail.com>
2023-06-28 13:49:08 +0530

    pratice

--- lawfirm/lawfirm/src/component/pratice/pratice.jsx
+++ lawfirm/lawfirm/src/component/pratice/pratice.jsx
@@ -1,8 +1,38 @@
 import "./pratice.css"
+import Image1 from "../../assets/img1.png"
+import Image2 from "../../assets/img2.png"
+import Image3 from "../../assets/img3.png"
+import Image4 from "../../assets/img4.png"
+import Image5 from "../../assets/img5.png"
+import Image6 from "../../assets/img6.png"
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
 
 export default function Pratice(){
     return(<div className="containerPratice">
-    
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <img src={Image1} alt="img1"/>
+        </Grid>
+        <Grid item xs={4}>
+        <img src={Image2} alt="img2"/>
+        </Grid>
+        <Grid item xs={4}>
+        <img src={Image3} alt="img3"/>
+        </Grid>
+        <Grid item xs={8}>
+        <img src={Image4} alt="img4"/>
+        </Grid>
+        <Grid item xs={8}>
+        <img src={Image5} alt="img5"/>
+        </Grid>
+        <Grid item xs={8}>
+        <img src={Image6} alt="img6"/>
+        </Grid>
+      </Grid>
+    </Box>
     
     </div>)
 }
