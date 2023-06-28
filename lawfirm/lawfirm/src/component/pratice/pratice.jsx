2df105d41eace95e9da427be63150ab8e274e808
rawahulislam <8715096926.abd@gmail.com>
2023-06-28 14:15:06 +0530

    MUiadded

--- lawfirm/lawfirm/src/component/pratice/pratice.jsx
+++ lawfirm/lawfirm/src/component/pratice/pratice.jsx
@@ -11,25 +11,26 @@ import Grid from '@mui/material/Grid';
 
 export default function Pratice(){
     return(<div className="containerPratice">
+      <div className="text1">Area of Pratice</div>
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
+      <Grid container spacing={3}>
         <Grid item xs={8}>
-          <img src={Image1} alt="img1"/>
+          <img src={Image6} alt="img1"/>
         </Grid>
         <Grid item xs={4}>
-        <img src={Image2} alt="img2"/>
+        <img src={Image5} alt="img2"/>
         </Grid>
         <Grid item xs={4}>
-        <img src={Image3} alt="img3"/>
+        <img src={Image4} alt="img3"/>
         </Grid>
         <Grid item xs={8}>
-        <img src={Image4} alt="img4"/>
+        <img src={Image3} alt="img4"/>
         </Grid>
         <Grid item xs={8}>
-        <img src={Image5} alt="img5"/>
+        <img src={Image2} alt="img5"/>
         </Grid>
-        <Grid item xs={8}>
-        <img src={Image6} alt="img6"/>
+        <Grid item xs={4}>
+        <img src={Image1} alt="img6"/>
         </Grid>
       </Grid>
     </Box>
