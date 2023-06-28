b0582d14db41b3c3107de9890abfd14e1c3c98f5
rawahulislam <8715096926.abd@gmail.com>
2023-06-28 15:16:38 +0530

    updateLast

--- lawfirm/lawfirm/src/component/pratice/pratice.jsx
+++ lawfirm/lawfirm/src/component/pratice/pratice.jsx
@@ -19,13 +19,13 @@ export default function Pratice() {
       </div>
       <Box sx={{ flexGrow: 1, marginBottom:"6rem"}}>
         <Grid container spacing={3}>
-          <Grid item xs={8}>
+          <Grid item xs={8} style={{cursor:"pointer"}}>
             <img src={Image6} alt="img1" />
             <span style={{ position: "absolute", top: "31rem", left: "1rem" }}>
               Business Law
             </span>
           </Grid>
-          <Grid item xs={4}>
+          <Grid item xs={4} style={{cursor:"pointer"}}>
             <img src={Image5} alt="img2" />
             <span
               style={{ position: "absolute", top: "31rem", left: "49.5rem" }}
@@ -33,13 +33,13 @@ export default function Pratice() {
               Partnersip Law
             </span>
           </Grid>
-          <Grid item xs={4}>
+          <Grid item xs={4} style={{cursor:"pointer"}}>
             <img src={Image4} alt="img3" />
             <span style={{ position: "absolute", top: "54rem", left: "1rem" }}>
               Real Estate Law
             </span>
           </Grid>
-          <Grid item xs={8}>
+          <Grid item xs={8} style={{cursor:"pointer"}}>
             <img src={Image3} alt="img4" />
             <span
               style={{ position: "absolute", top: "54rem", left: "25.5rem" }}
@@ -47,13 +47,13 @@ export default function Pratice() {
               Business Law
             </span>
           </Grid>
-          <Grid item xs={8}>
+          <Grid item xs={8} style={{cursor:"pointer"}}>
             <img src={Image2} alt="img5" />
             <span style={{ position: "absolute", top: "77rem", left: "1rem" }}>
               Landlord Disputes
             </span>
           </Grid>
-          <Grid item xs={4}>
+          <Grid item xs={4} style={{cursor:"pointer"}}>
             <img src={Image1} alt="img6" />
             <span
               style={{ position: "absolute", top: "77rem", left: "49.5rem" }}
